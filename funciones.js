013661bd3d4235856fbd1d1385ad867d0abc8654
Jamer Rq <jamerrq@gmail.com>
2023-03-09 09:07:29 -0500

    added functions files

---
+++ funciones.js
@@ -0,0 +1,21 @@
+// function sumaTres(x){
+//     console.log(x + 3);
+// }
+
+// sumaTres(5);
+
+
+// Primer manera de declarar una función
+function sumaTres(x){
+    return x + 3;
+}
+
+// Segunda
+var sumaTres = function(x){
+    return x + 3;
+};
+
+// Tercera
+var sumaTres = (x) => {
+    return x + 3;
+}
---
+++ funciones2.js
@@ -0,0 +1,11 @@
+function cuidadoConElConsoleLog(nombre){
+    console.log(nombre);
+    return nombre;
+}
+
+function otraFuncion(){
+    return (
+        "El nombre retornado por la funcion cuidadoConElConsoleLog es: " +
+        cuidadoConElConsoleLog("Camilo")
+    );
+}
