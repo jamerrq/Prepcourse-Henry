490ec80512141ca128b47e616bb194afa25eee5e
Jamer Rq <jamerrq@gmail.com>
2023-03-09 08:26:59 -0500

    created file variables.js

---
+++ Variables.js
@@ -0,0 +1,19 @@
+var cajon;
+
+// Strings
+var bootcamp = "Henry";
+
+// Números
+var numeroEntero = 7;
+var numeroNegativo = -4;
+var numeroDecimal = 11.75;
+
+// Booleanos
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefined
+console.log(cajon);
+
+// NULL
+var sinNada = null;
