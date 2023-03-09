469cd4c3d12dc7a179ac3bf22a7aeff36145f537
Jamer Rq <jamerrq@gmail.com>
2023-03-09 08:48:03 -0500

    added file operadoresDeComparacion.js

---
+++ operadoresDeComparacion.js
@@ -0,0 +1,22 @@
+// console.log(4 > 7); // mayor que
+// console.log(4 < 7); // menor que
+// console.log(4 >= 7); // mayor o igual que
+// console.log(4 <= 7); // menor o igual que
+
+
+// Doble == para comparar valores
+// console.log(4 == "4");
+
+// Triple === para comparar tanto valor como tipo de dato
+// console.log(4 === "4");
+// console.log(4 === 4);
+
+
+// Asociatividad hacia la derecha
+var a = 1;
+var b = 2;
+var c = (a = b);
+
+console.log(a);
+console.log(b);
+console.log(c);
