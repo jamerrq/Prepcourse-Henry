b893acc323f987516c1ed1bc7b543f069c9a018b
Jamer Rq <jamerrq@gmail.com>
2023-03-09 23:08:35 -0500

    added prototipos file

---
+++ prototipos.js
@@ -0,0 +1,28 @@
+Array.prototype.mayoresQueTres = function(){
+    var arregloModificado = [];
+    for(var i = 0; i < this.length; ++i){
+        if(this[i] > 3)arregloModificado.push(false);
+        else arregloModificado.push(this[i]);
+    }
+    return arregloModificado;
+}
+
+var arreglo = [1, 2, 3, 4, 5];
+var nuevoArreglo = arreglo.mayoresQueTres();
+// console.log(nuevoArreglo);
+
+
+
+class LatinoAmerica{
+    constructor(){
+        this.paises = [];
+    }
+}
+
+LatinoAmerica.prototype.agregarPais = function(pais){
+    this.paises.push(pais);
+}
+
+var continente = new LatinoAmerica();
+continente.agregarPais("México");
+console.log(continente.paises);
